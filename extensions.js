66b90442b21be8e6bd73cad82380786e551a2d69
King3 <75344974+coderking3@users.noreply.github.com>
2025-08-08 01:33:02 +0800

    add my profile

---
+++ extensions.js
@@ -0,0 +1,123 @@
+const EXTENSIONS_PROFILE = {
+  // 通用配置（按需安装）
+  base: [
+    'Auto Complete Tag',
+    'Auto Import',
+    'Better Comments',
+    'browserslist',
+    'Catppuccin for VSCode',
+    'Chinese (Simplified) (简体中文) Language Pack for Visual Studio Code',
+    'Code Spell Checker',
+    'Color Highlight',
+    'Colorful Comments',
+    'CSS Peek',
+    'CSS Variable Autocomplete',
+    'Document This',
+    'DotENV',
+    'EditorConfig for VS Code',
+    'Error Lens',
+    'ES7 React/Redux/GraphQL/React-Native snippets',
+    'ESLint',
+    'Feather Product Icons',
+    'Fix VSCode Checksums Next',
+    'Git Graph',
+    'Git History',
+    'GitLens — Git supercharged',
+    'Highlight Matching Tag',
+    'HTML CSS Support',
+    'HTML-Comment',
+    'iconfont-preview',
+    'Image preview',
+    'IntelliCode',
+    'IntelliCode API Usage Examples',
+    'JavaScript (ES6) code snippets',
+    'JavaScript and TypeScript Nightly',
+    'JavaScript Debugger (Nightly)',
+    'Laravel goto view',
+    'Markdown All in One',
+    'npm Intellisense',
+    'Path Autocomplete',
+    'Path Intellisense',
+    'Prettier - Code formatter',
+    'Svg Preview',
+    'Template String Converter',
+    'Turbo Console Log',
+    'VSCode Great Icons',
+    'WakaTime',
+    // 代码提示
+    'Tabnine AI',
+    'YAML',
+
+    /* 按需安装 */
+    // 效率
+    'Bookmarks',
+    // ps1脚本相关
+    'PowerShell',
+    // css相关
+    'Sass (.sass only)',
+    'SCSS Formatter',
+    'SCSS IntelliSense',
+    'PostCSS Language Support',
+    'Stylelint',
+    'px to rem & rpx & vw (cssrem)', // react,vue,wechatApp,uniApp
+    'UnoCSS',
+    'Tailwind CSS IntelliSense',
+    // 测试相关
+    'Jest',
+    'Jest Runner',
+    'Jest Snippets',
+    'Vitest',
+    // 打开html
+    'open in browser',
+    'Live Server',
+    // 其他
+    'Iconify IntelliSense',
+  ],
+  vue: ['Vue - Official', 'Vue 3 Snippets', 'Vue VSCode Snippets'],
+  react: [
+    'ES7+ React/Redux/React-Native snippets',
+    'Nextjs snippets',
+    'JS JSX Snippets',
+    'Simple React Snippets',
+    'Typescript React code snippets',
+    'VSCode React Refactor',
+  ],
+  nodejs: [
+    // 容器相关
+    'Docker',
+    'Dev Containers',
+    'Kubernetes',
+    // 注释类
+    'koroFileHeader',
+    // support
+    'node-snippets',
+    'Express Snippets',
+    'EJS language support',
+    'REST Client',
+    'XML',
+    // 数据库相关
+    'MongoDB for VS Code',
+    // 漏洞
+    'Red Hat Dependency Analytics',
+    // 引入包大小
+    'import cost',
+  ],
+  uniApp: [
+    // uniapp 框架支持
+    'uniapp小程序扩展',
+    'uni-create-view',
+    'uni-helper',
+    // vue3 语法支持
+    'Vue - Official',
+    'Vue 3 Snippets',
+    'Vue VSCode Snippets',
+  ],
+  wechatApp: [
+    '小程序开发助手',
+    'vscode wxml',
+    'WXML - Language Service',
+    'vscode weapp api',
+  ],
+}
+
+export { EXTENSIONS_PROFILE }
